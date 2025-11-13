d824b755bfffb6c8e1bbcf2c25f0fd4384c0cb27
MayurSMahajan <mayur23smahajan@gmail.com>
2025-11-13 17:26:31 +0530

    refactor: set up github pages

--- any-carousal/apps/web/next.config.js
+++ any-carousal/apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    output: 'export', // Required for static export
+    images: {
+        unoptimized: true, // GitHub Pages doesn’t support Next.js image optimization
+    },
+    basePath: isProd ? '/AnyCarousel' : '',
+    assetPrefix: isProd ? '/AnyCarousel/' : '',
+};
 
 export default nextConfig;
